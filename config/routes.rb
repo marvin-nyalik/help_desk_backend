227ff94886d09cf97a1fef812540f591395ef14a
MARVIN <o.silverygold@gmail.com>
2025-04-21 19:11:31 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  },
+  defaults: { format: :json }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+
+  resources :tickets, param: :id do
+    member do
+      patch :close
+      patch :reopen
+      patch :assign
+    end
+  end
+
+  resources :notifications, only: [:index] do
+    member do
+      patch :mark_as_read
+    end
+  end
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
 end
